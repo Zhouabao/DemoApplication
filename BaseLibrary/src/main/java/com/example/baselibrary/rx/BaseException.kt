feef44a9b239bf5cd214216f8acce5491dae420e
zfm <1535154542@qq.com>
2019-08-22 17:21:08 +0800

    解决403问题

--- BaseLibrary/src/main/java/com/example/baselibrary/rx/BaseException.kt
+++ BaseLibrary/src/main/java/com/example/baselibrary/rx/BaseException.kt
@@ -1,6 +1,8 @@
 package com.kotlin.base.rx
 
+import java.io.IOException
+
 /*
     定义通用异常
  */
-class BaseException(val code: Int, val msg: String) : Throwable()
+class BaseException(val code: Int, val msg: String) : IOException()
