6c61dc841f2809a24bb5642bbd62bbb13ffdd21b
zfm <1535154542@qq.com>
2019-09-04 10:10:02 +0800

    图片大小修改

--- InterestApplication/src/main/java/com/example/demoapplication/ui/adapter/SquareDetailImgsAdaper.kt
+++ InterestApplication/src/main/java/com/example/demoapplication/ui/adapter/SquareDetailImgsAdaper.kt
@@ -1,14 +1,15 @@
 package com.example.demoapplication.ui.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.viewpager.widget.PagerAdapter
-import com.blankj.utilcode.util.ScreenUtils
 import com.example.baselibrary.glide.GlideUtil
-import com.example.baselibrary.widgets.RoundImageView
+import com.example.demoapplication.R
 import com.example.demoapplication.model.VideoJson
+import kotlinx.android.synthetic.main.item_detail_img_viewpager.view.*
 
 /**
  *    author : ZFM
@@ -26,17 +27,22 @@ class SquareDetailImgsAdaper(val context: Context, val datas: MutableList<VideoJ
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val imageview = RoundImageView(context)
-        imageview.setType(RoundImageView.TYPE_NORMAL)
-        imageview.transitionName = "imageview"
-        imageview.layoutParams = ViewGroup.LayoutParams(ScreenUtils.getScreenWidth(), ScreenUtils.getScreenHeight())
-        GlideUtil.loadImg(context, datas[position].url, imageview)
-        container.addView(imageview)
-        return imageview
+        val view = LayoutInflater.from(context).inflate(R.layout.item_detail_img_viewpager, null)
+        view.detailImg.transitionName = "imageview"
+        GlideUtil.loadRoundImgCenterinside(context, datas[position].url, view.detailImg, 1F, 0)
+
+
+
+//        val imageview = RoundImageView(context)
+//        imageview.setType(RoundImageView.TYPE_NORMAL)
+//        imageview.layoutParams = ViewGroup.LayoutParams(ScreenUtils.getScreenWidth(), ScreenUtils.getScreenHeight())
+//        imageview.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        container.addView(view)
+        return view
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        val imageView = `object` as ImageView
+        val imageView = `object` as LinearLayout
         container.removeView(imageView)
     }
 }
