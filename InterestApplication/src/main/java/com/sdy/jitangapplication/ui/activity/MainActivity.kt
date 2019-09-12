ca5dc445334b232e2b9719316727dfad33a88c05
zfm <1535154542@qq.com>
2019-09-12 15:02:20 +0800

    键盘收起修改

--- InterestApplication/src/main/java/com/sdy/jitangapplication/ui/activity/MainActivity.kt
+++ InterestApplication/src/main/java/com/sdy/jitangapplication/ui/activity/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.blankj.utilcode.util.AppUtils
+import com.blankj.utilcode.util.KeyboardUtils
 import com.blankj.utilcode.util.SPUtils
 import com.blankj.utilcode.util.ToastUtils
 import com.daimajia.androidanimations.library.Techniques
@@ -330,7 +331,18 @@ class MainActivity : BaseMvpActivity<MainPresenter>(), MainView, View.OnClickLis
 
     override fun onResume() {
         super.onResume()
+        if (KeyboardUtils.isSoftInputVisible(this))
+            KeyboardUtils.hideSoftInput(this)
         parseIntents()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
     }
 
     override fun onDestroy() {
