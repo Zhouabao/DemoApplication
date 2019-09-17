0aba127df98f493a181d889c095fa1f80b8f6e1a
zfm <1535154542@qq.com>
2019-09-17 18:58:41 +0800

    会员支付提示修改

--- InterestApplication/src/main/java/com/sdy/jitangapplication/ui/dialog/ChargeVipDialog.kt
+++ InterestApplication/src/main/java/com/sdy/jitangapplication/ui/dialog/ChargeVipDialog.kt
@@ -217,11 +217,17 @@ class ChargeVipDialog(val context1: Context) : Dialog(context1, R.style.MyDialog
                         if (TextUtils.equals(resultStatus, "9000")) {
                             // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
                             showAlert(context1, "支付成功！")
+                        } else if (TextUtils.equals(resultStatus, "8000")) {
+                            // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                            showAlert(context1, "支付结果确认中！")
+                        } else if (TextUtils.equals(resultStatus, "6001")) {
+                            // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                            showAlert(context1, "支付取消！")
                         } else {
                             // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
                             showAlert(context1, "支付失败！")
-
                         }
+
                     }
                 }
             }
