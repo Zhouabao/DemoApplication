7a18b9184f638ed6c702ee392086db39f2471e70
zfm <1535154542@qq.com>
2019-10-14 14:26:37 +0800

    详情界面显示与隐藏文本内容 方便查看大图

--- InterestApplication/src/main/java/com/sdy/jitangapplication/ui/adapter/SquareDetailImgsAdaper.kt
+++ InterestApplication/src/main/java/com/sdy/jitangapplication/ui/adapter/SquareDetailImgsAdaper.kt
@@ -4,15 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.viewpager.widget.PagerAdapter
-import com.kotlin.base.ext.onClick
 import com.sdy.baselibrary.glide.GlideUtil
 import com.sdy.jitangapplication.R
 import com.sdy.jitangapplication.model.SquareBean
 import com.sdy.jitangapplication.model.VideoJson
 import com.sdy.jitangapplication.ui.activity.SquarePlayListDetailActivity
-import kotlinx.android.synthetic.main.item_detail_img_viewpager.view.*
+import uk.co.senab.photoview.PhotoView
 
 /**
  *    author : ZFM
@@ -20,7 +18,7 @@ import kotlinx.android.synthetic.main.item_detail_img_viewpager.view.*
  *    desc   :
  *    version: 1.0
  */
-class SquareDetailImgsAdaper(val context: Context, val datas: MutableList<VideoJson>, val position: Int = 0) :
+class SquareDetailImgsAdaper(val context: Context, val datas: MutableList<VideoJson>, val coverPosition: Int = 0) :
     PagerAdapter() {
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
         return view == `object`
@@ -31,26 +29,24 @@ class SquareDetailImgsAdaper(val context: Context, val datas: MutableList<VideoJ
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_detail_img_viewpager, null)
-        view.detailImg.transitionName = "imageview"
-        GlideUtil.loadRoundImgCenterinside(context, datas[position].url, view.detailImg, 1F, 0)
-        view.onClick {
+        val detailImg = LayoutInflater.from(context).inflate(R.layout.item_detail_img_viewpager, null) as PhotoView
+        detailImg.transitionName = "imageview"
+        GlideUtil.loadRoundImgCenterinside(context, datas[position].url, detailImg, 1F, 0)
+        detailImg.setOnPhotoTapListener { _, x, y ->
             if (context is SquarePlayListDetailActivity) {
-                context.hideCover(position, SquareBean.PIC)
+                context.hideCover(coverPosition, SquareBean.PIC)
             }
         }
-
-
 //        val imageview = RoundImageView(context)
 //        imageview.setType(RoundImageView.TYPE_NORMAL)
 //        imageview.layoutParams = ViewGroup.LayoutParams(ScreenUtils.getScreenWidth(), ScreenUtils.getScreenHeight())
 //        imageview.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-        container.addView(view)
-        return view
+        container.addView(detailImg)
+        return detailImg
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        val imageView = `object` as LinearLayout
+        val imageView = `object` as PhotoView
         container.removeView(imageView)
     }
 }
