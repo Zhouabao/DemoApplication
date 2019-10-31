72a532350d9c59ba0b696d67a702d2561373c007
zfm <1535154542@qq.com>
2019-10-31 11:42:34 +0800

    cardstackview lib 加入并修改滑动率

---
+++ InterestApplication/src/main/java/com/sdy/jitangapplication/widgets/cardstackview/StackFrom.java
@@ -0,0 +1,13 @@
+package com.sdy.jitangapplication.widgets.cardstackview;
+
+public enum StackFrom {
+    None,
+    Top,
+    TopAndLeft,
+    TopAndRight,
+    Bottom,
+    BottomAndLeft,
+    BottomAndRight,
+    Left,
+    Right,
+}
