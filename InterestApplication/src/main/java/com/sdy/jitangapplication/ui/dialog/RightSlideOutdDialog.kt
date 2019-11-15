b311828005922d248effcadbdbf0cfd32434dd68
zfm <1535154542@qq.com>
2019-11-15 19:50:30 +0800

    右滑次数用尽弹窗

--- InterestApplication/src/main/java/com/sdy/jitangapplication/ui/dialog/RightSlideOutdDialog.kt
+++ InterestApplication/src/main/java/com/sdy/jitangapplication/ui/dialog/RightSlideOutdDialog.kt
@@ -2,9 +2,12 @@ package com.sdy.jitangapplication.ui.dialog
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.Gravity
 import android.view.WindowManager
+import com.blankj.utilcode.util.SpanUtils
 import com.kotlin.base.ext.onClick
 import com.sdy.jitangapplication.R
 import com.sdy.jitangapplication.ui.activity.NewUserInfoSettingsActivity
@@ -17,7 +20,8 @@ import org.jetbrains.anko.startActivity
  *    desc   : 右滑次数用尽dialog
  *    version: 1.0
  */
-class RightSlideOutdDialog(val myContext: Context) : Dialog(myContext, R.style.MyDialog) {
+class RightSlideOutdDialog(val myContext: Context, var myCount: Int = 0, var maxCount: Int = 0) :
+    Dialog(myContext, R.style.MyDialog) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,19 @@ class RightSlideOutdDialog(val myContext: Context) : Dialog(myContext, R.style.M
         slideOutClose.onClick {
             dismiss()
         }
+
+
+        slideOutMin.typeface = Typeface.createFromAsset(myContext.assets, "DIN_Alternate_Bold.ttf")
+        slideOutMin.text = "${myCount}"
+        slideOutMax.typeface = Typeface.createFromAsset(myContext.assets, "DIN_Alternate_Bold.ttf")
+        slideOutMax.text = "${maxCount}"
+        slideOutContent.text = SpanUtils.with(slideOutContent)
+            .append("现在去完善资料免费右滑次数将从每天${myCount}次增至每天")
+            .append("$maxCount")
+            .setForegroundColor(myContext.resources.getColor(R.color.colorOrange))
+            .append("次")
+            .setForegroundColor(Color.parseColor("#B6BABF"))
+            .create()
 
     }
 
