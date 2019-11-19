a4948f0d8d82cf2e1bb5ed1bba23a87e1247530f
zfm <1535154542@qq.com>
2019-11-19 20:37:19 +0800

    发布改为单标签

--- InterestApplication/src/main/java/com/sdy/jitangapplication/model/LoginBean.kt
+++ InterestApplication/src/main/java/com/sdy/jitangapplication/model/LoginBean.kt
@@ -21,7 +21,7 @@ data class Userinfo(
     val nickname: String? = "",
     val avatar: String? = "",
     val gender: Int = 0,
-    val birth: String = "",
+    val birth: Int = 0,
     val accid: String = "",
     val allvisit: Int = 0,
     val face_audit_state: Int? = 0,
