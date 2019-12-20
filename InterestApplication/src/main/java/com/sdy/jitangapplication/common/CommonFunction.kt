cd784100479a4b037187647933e30efdb7c034b4
zfm <1535154542@qq.com>
2019-12-20 20:43:57 +0800

    打招呼逻辑修改

--- InterestApplication/src/main/java/com/sdy/jitangapplication/common/CommonFunction.kt
+++ InterestApplication/src/main/java/com/sdy/jitangapplication/common/CommonFunction.kt
@@ -8,22 +8,32 @@ import android.view.View
 import com.blankj.utilcode.util.ActivityUtils
 import com.blankj.utilcode.util.NetworkUtils
 import com.blankj.utilcode.util.ToastUtils
+import com.kotlin.base.data.net.RetrofitFactory
+import com.kotlin.base.data.protocol.BaseResp
+import com.kotlin.base.ext.excute
+import com.kotlin.base.rx.BaseException
+import com.kotlin.base.rx.BaseSubscriber
 import com.luck.picture.lib.PictureSelector
 import com.luck.picture.lib.config.PictureConfig
 import com.netease.nimlib.sdk.NIMClient
 import com.netease.nimlib.sdk.msg.MsgService
 import com.netease.nimlib.sdk.msg.constant.SessionTypeEnum
 import com.sdy.jitangapplication.R
+import com.sdy.jitangapplication.api.Api
+import com.sdy.jitangapplication.event.GreetEvent
+import com.sdy.jitangapplication.model.GreetBean
 import com.sdy.jitangapplication.nim.activity.ChatActivity
 import com.sdy.jitangapplication.ui.activity.MainActivity
 import com.sdy.jitangapplication.ui.dialog.ChargeVipDialog
 import com.sdy.jitangapplication.ui.dialog.HarassmentDialog
 import com.sdy.jitangapplication.ui.dialog.SayHiDialog
+import com.sdy.jitangapplication.ui.dialog.TickDialog
 import com.sdy.jitangapplication.utils.GlideEngine
 import com.sdy.jitangapplication.utils.UriUtils
 import com.sdy.jitangapplication.utils.UserManager
 import com.tencent.mm.opensdk.modelmsg.SendAuth
 import com.tencent.mm.opensdk.openapi.WXAPIFactory
+import org.greenrobot.eventbus.EventBus
 
 /**
  *    author : ZFM
@@ -64,24 +74,64 @@ object CommonFunction {
         view: View
     ) {
         view.isEnabled = false
-        if (isfriend || isgreeted) {
-            ChatActivity.start(context, targetAccid)
-        } else {
-            if (UserManager.getLightingCount() > 0) {
-                if (!greet_switch) {
-                    toast("对方已关闭招呼功能")
-                } else {
-                    if (greet_state && UserManager.isUserVerify() != 1) {
-                        HarassmentDialog(context, HarassmentDialog.CHATHI).show()
+        /**
+         * 判断当前能否打招呼
+         */
+        if (!NetworkUtils.isConnected()) {
+            toast("请连接网络！")
+            view.isEnabled = true
+            return
+        }
+
+        val params = UserManager.getBaseParams()
+        params["target_accid"] = targetAccid
+        RetrofitFactory.instance.create(Api::class.java)
+            .greetState(UserManager.getSignParams(params))
+            .excute(object : BaseSubscriber<BaseResp<GreetBean?>>(null) {
+                override fun onStart() {
+                }
+
+                override fun onNext(t: BaseResp<GreetBean?>) {
+                    if (t.code == 200) {
+                        val greetBean = t.data
+                        if (greetBean != null && greetBean.lightningcnt != -1) {
+                            if (greetBean.isfriend || greetBean.isgreet) {
+                                ChatActivity.start(context, targetAccid)
+                            } else {
+                                if (UserManager.getLightingCount() > 0) {
+                                    if (!greet_switch) {
+                                        toast("对方已关闭招呼功能")
+                                    } else {
+                                        if (greet_state && UserManager.isUserVerify() != 1) {
+                                            HarassmentDialog(context, HarassmentDialog.CHATHI).show()
+                                        } else {
+                                            SayHiDialog(targetAccid, targetNickName, context).show()
+                                        }
+                                    }
+                                } else {
+                                    ChargeVipDialog(ChargeVipDialog.DOUBLE_HI, context, ChargeVipDialog.PURCHASE_GREET_COUNT).show()
+                                }
+                            }
+
+                        } else {
+                            toast(t.msg)
+                        }
                     } else {
-                        SayHiDialog(targetAccid, targetNickName, context).show()
+                        EventBus.getDefault().post(GreetEvent(context, true))
+                        toast(t.msg)
+                    }
+                    view.postDelayed({ view.isEnabled = true }, 500L)
+                }
+
+                override fun onError(e: Throwable?) {
+                    view.isEnabled = true
+                    if (e is BaseException) {
+                        TickDialog(context).show()
                     }
                 }
-            } else {
-                ChargeVipDialog(ChargeVipDialog.DOUBLE_HI, context, ChargeVipDialog.PURCHASE_GREET_COUNT).show()
-            }
-        }
-        view.postDelayed({ view.isEnabled = true }, 500L)
+            })
+
+
     }
 
 
@@ -107,7 +157,7 @@ object CommonFunction {
         val wxapi = WXAPIFactory.createWXAPI(context, null)
         wxapi.registerApp(Constants.WECHAT_APP_ID)
         if (!wxapi.isWXAppInstalled) {
-            CommonFunction.toast("你没有安装微信")
+            toast("你没有安装微信")
             return
         }
         val req = SendAuth.Req()
